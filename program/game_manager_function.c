f5558062b7e99d6094ab5b6964c52b1c800f27e9
Lear-kao <axelaudeoud66@gmail.com>
2025-04-21 22:23:40 +0200

    jkl

--- program/game_manager_function.c
+++ program/game_manager_function.c
@@ -186,6 +186,7 @@ void player_turn(char playerNumber, struct list_player *p_list, struct Stack **p
         {
             pose = 1;
             *leftTopGrid = place_tile(leftTopGrid, play_grid[token - 1]->coord, turn_tile, dllist, hauteur, largeur); // token -1 car 0 correspond à tourner la tuile
+            p_list->player[playerNumber]->coulPlayer
             pointPlacedTile(play_grid[token - 1], listPlayer); //besoin de la fonction de théo
         }
     }
--- program/player.c
+++ program/player.c
@@ -42,10 +42,10 @@ struct list_player *init_player_list(char nbbot) // Axel
     for( int i = 0; i < nbPlayers; i++ )
     {
         if(i>nbPlayers-nbbot-1){
-            list_players->player[i]=init_player(i,1);
+            list_players->player[i]=init_player(i+1,1);
         }
         else{
-            list_players->player[i]=init_player(i,0);
+            list_players->player[i]=init_player(i+1,0);
         }
     }
     list_players->player[nbPlayers] = NULL;
@@ -68,3 +68,23 @@ void reset_meeples(struct Player *player) // Fait
 {
     player->nbMeeple = NBMEEPLE_DEFAULT;
 }
+
+
+//fonction pas finie je dois faire bcp de fct  à côté
+
+/* void put_meeple(struct Grid* grid, struct list_player *p_list, char pnumber)
+{
+    //if (!can_place_meeple()){ printf("vous ne pouvez pas poser de meeple\n");return;}
+    if (!is_meeple_on_player(p_list->player[pnumber]))
+    {
+        return;
+    }
+    printf("voulez vous poser un meeple ? (0 : Non, 1 : Oui)");
+    int temp;
+    scanf("%d",temp);
+    if(temp) return;
+    printf("ou voulez vous poser votre meeple (0 = Droite, 1 = Haut, 2 = Gauche, 3 = Bas)");
+    scanf("%c",grid->tile->meeplePlace);
+    grid->tile->meeple = p_list->player[pnumber];
+    return;
+} */
